bc035f56a425b7420f0a4f8318efbae622a73280
Rowe.Luo <>
2012-10-07 20:49:57 +0800

    Rowe.Luo

--- protected/controller/CatalogController.php
+++ protected/controller/CatalogController.php
@@ -1,30 +1,102 @@
 <?php
 
+Doo::loadModel('Catalog');
+Doo::loadHelper('DooValidator');
+Doo::loadHelper('DooUrlBuilder');
+
 class CatalogController extends DooController {
 
-	function index() {
-		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
-	}
+    function index() {
+        $catalogInstance = new Catalog();
+        //$catalogInstance->index_children_catalog();
 
-	function create() {
-		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
-	}
 
-	function edit() {
-		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
-	}
+        $catalogInstanceList = $catalogInstance->index_all_catalog();
+        echo '<br/>--------------------------------------------------------------------------<br/>';
 
-	function remove() {
-		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
-	}
+        foreach ($catalogInstanceList as $var_catalog) {
 
-	function save() {
-		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
-	}
+            echo '<br/>id=' . $var_catalog->id;
+            echo '<br/>parent_id=' . $var_catalog->parent_id;
+            echo '<br/>name=' . $var_catalog->name;
+            echo '<br/>--------------------------------------------------------------------------<br/>';
+        }
 
-	function update() {
-		echo 'You are visiting '.$_SERVER['REQUEST_URI'];
-	}
+
+        echo $this->toJSON($catalogInstanceList);
+
+        echo $this->toXML($catalogInstanceList);
+    }
+
+    function create() {
+
+        $data['root_url'] = Doo::conf()->APP_URL . 'index.php';
+
+        //Doo::logger()->debug("创建新的目录,参数为：".  implode(",", $_GET));
+        $parent_id = 0;
+        if (isset($_GET['parent_id'])) {
+            $parent_id = $_GET['parent_id'];
+        }
+
+
+        $catalogInstance = new Catalog;
+
+        $catalogInstance->parent_id = $parent_id;
+
+
+
+        $data['catalogInstance'] = $catalogInstance;
+
+        $this->renderc('/admin/catalog/create', $data);
+    }
+
+    function edit() {
+        echo 'You are visiting ' . $_SERVER['REQUEST_URI'];
+    }
+
+    function remove() {
+        echo 'You are visiting ' . $_SERVER['REQUEST_URI'];
+    }
+
+    function save() {
+        
+        $parent_id = 0;
+        if (isset($_POST['parent_id'])) {
+            $parent_id = $_POST['parent_id'];
+        }
+        
+        $catalogInstance = new Catalog;
+
+        $catalogInstance->parent_id = $parent_id;
+
+
+
+        $data['catalogInstance'] = $catalogInstance;
+
+        $v = new DooValidator();
+
+        $validate_rules = array(
+            'name' => array('email', '请正确输入邮件地址！'),
+            'description' => array('description', 6, 200, '密码支持数字，字母与下划线！'),
+            'img_path' => array('img_path', 6, 200, '密码支持数字，字母与下划线！'),
+        );
+
+        if ($error = $v->validate($_POST, $validate_rules)) {
+
+
+            Doo::logger()->info('用户登录输入的数据验证失败!');
+
+            $data['form_error'] = $error;
+            $this->renderc('user/index', $data, true);
+            return;
+        }
+ 
+    }
+
+    function update() {
+        echo 'You are visiting ' . $_SERVER['REQUEST_URI'];
+    }
 
 }
+
 ?>
--- protected/model/Catalog.php
+++ protected/model/Catalog.php
@@ -4,11 +4,29 @@ Doo::loadModel('base/CatalogBase');
 class Catalog extends CatalogBase{
     
     public function index_all_catalog(){
+        Doo::logger()->info("获取所有的目录信息");
+        
+        $catalogInstanceList=  $this->find();
+        
+        return $catalogInstanceList;
+        
         
     }
     
     
     public function index_children_catalog($parent_id=0){
+        Doo::logger()->info("根据父目录ID=".$parent_id."获取目录信息！");
+       
+        
+        //条件
+        $opt=array('parent_id'=>$parent_id);
+        
+        $catalogList=$this->find($opt);
+        
+        var_dump($catalogList);
+        
+        return $catalogList;
+        
         
     }
 }
